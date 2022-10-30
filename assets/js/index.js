1b8d5d58813b7605d8f04990f765ca0f7558a781
baalejandro <abample@gmail.com>
2022-10-30 16:09:49 +0100

    text integration in slider

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,12 @@
 const images = document.querySelectorAll('.gallery-image');
 let count = 0;
+let speed = 8000;
 
-const slideshow = () => {
-  images[count].style.display = 'block';
+const slideshow = async () => {
   let previous = ((count - 1) == -1) ? images.length - 1 : count - 1;
   images[previous].style.display = 'none';
+  images[count].style.display = 'block';
   count === images.length - 1 ? count = 0 : count++;
 }
 
-const interval = window.setInterval(() => slideshow(), 3000);
+const interval = window.setInterval(() => slideshow(), speed);
